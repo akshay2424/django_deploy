83ee069dbf925ef753e336eacaa5f97e4385efad
Akshay <akshayrandhave2424@gmail.com>
2025-11-01 12:02:54 +0530

    Add Django CI workflows

--- api/views.py
+++ api/views.py
@@ -2,14 +2,15 @@ from rest_framework import viewsets, generics
 from .models import Post
 from django.contrib.auth.models import User
 from .serializers import PostSerializer, RegisterSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 class RegisterViewSet(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permissions_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
